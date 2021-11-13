b2ce7a6d825d9db49306ccf88aeff2ab04bcd825
Simon Mießler <simon.miessler@gmx.net>
2021-11-13 01:03:33 +0100

    ...

--- restore_key.cmd
+++ restore_key.cmd
@@ -2,5 +2,5 @@
 @echo off
 
 sig\crypt.exe decrypt sig\sns key.snk
-sig\crypt.exe enablesigning src\MMQ\MMQ.csproj ..\key.snk
-sig\crypt.exe enablesigning src\MMQ.Test\MMQ.Test.csproj ..\key.snk netstandard
+sig\crypt.exe enablesigning src\MMQ\MMQ.csproj ..\..\key.snk
+sig\crypt.exe enablesigning src\MMQ.Test\MMQ.Test.csproj ..\..\key.snk netstandard
