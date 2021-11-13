6da323a96e1ce619bfdce3887f7c42d0a3983d6d
Simon Mießler <kittyfisto87@gmail.com>
2021-11-13 01:07:43 +0100

    Refactored project structure (#1)
    * Refactored project structure
    
    - Removed old .NET style projects
    - Made use of TargetFrameworks to compile against multiple frameworks
    
    * Fixing release build & nuget packaging
    
    * ...

--- src/MMQ/Properties/AssemblyInfo.cs
+++ src/MMQ/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MMQ.Test")]
+[assembly: AssemblyTitle("MMQ")]
 [assembly: AssemblyDescription("Memory Mapped Queue")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MMQ.Test")]
-[assembly: AssemblyCopyright("Copyright © Simon Mießler 2017")]
+[assembly: AssemblyProduct("MMQ")]
+[assembly: AssemblyCopyright("Copyright © Simon Mießler 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,17 +19,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e098041c-dc10-401f-a903-fc58c83e1f76")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("f0d39a0d-7bf0-46ba-a7f3-6fed179922c4")]
